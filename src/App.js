f9443db9959990305cb2bb8ab528a79cce6a245c
abenaesha <abenaesha@github.com>
2021-07-19 15:11:53 +0100

    finish header components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	)
+}
+
+export default App;
