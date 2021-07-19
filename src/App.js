80fc41dc22404a7c0033647f4a04ad895f86968e
abenaesha <abenaesha@github.com>
2021-07-19 17:19:32 +0100

    finish newUser and users components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React from "react"
 import Header from "./components/header/Header"
 import Sidebar from "./components/sidebar/Sidebar"
 import "./App.css"
-import Home from "./pages/Home"
+import Home from "./pages/home/Home"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import UserList from "./pages/userList/UserList"
+import User from "./pages/user/User"
+import NewUser from "./pages/newUser/NewUser"
 
 function App() {
 	return (
-		<div className="App">
+		<Router>
 			<Header />
 			<div className="container">
 				<Sidebar />
-				<Home />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/users">
+						<UserList />
+					</Route>
+					<Route path="/user/:userId">
+						<User />
+					</Route>
+					<Route path="/newUser">
+						<NewUser />
+					</Route>
+					{/* <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route> */}
+				</Switch>
 			</div>
-		</div>
+		</Router>
 	)
 }
 
